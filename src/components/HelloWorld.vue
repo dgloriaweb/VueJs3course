d369517d677ba177597fc72c3862fd55a2c7a16c
unknown <dgloria.web@gmail.com>
2021-01-14 03:23:55 +0000

    ep 8 pt 1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="container">
-      <h1>{{title}}</h1>
+      <h1>{{ title }}</h1>
       <div class="cart">Basic counter({{ counter }})</div>
       <div class="cart">Cart({{ cart }})</div>
       <!-- <div class="product-image"> -->
@@ -10,8 +10,8 @@
       <h1>{{ product }}</h1>
       <p>{{ description }}</p>
       <a :href="myPath" target="_blank">W3Schools</a>
-      <p v-if="inventory > 10">In Stock</p>
-      <p v-else-if="inventory <= 10 && inventory > 0">Low Stock</p>
+      <p v-if="inStock > 10">In Stock</p>
+      <p v-else-if="inStock <= 10 && inStock > 0">Low Stock</p>
       <p v-else>Out of Stock</p>
       <p v-if="onSale">OnSale</p>
       <h2>Sensitivities</h2>
@@ -28,9 +28,9 @@
       </ul>
       <h2>Variants</h2>
       <div
-        v-for="variant in variants"
+        v-for="(variant, index) in variants"
         :key="variant.id"
-        @mouseover="updateImage(variant.image)"
+        @mouseover="updateVariant(index)"
       >
         Size {{ variant.size }}
       </div>
@@ -49,7 +49,7 @@
         class="button"
         type="button"
         v-on:click="addToCart"
-        :class="{ disabledButton: !inventory }"
+        :class="{ disabledButton: !inStock }"
       >
         Add to Cart
       </button>
@@ -64,8 +64,8 @@ export default {
     addToCart() {
       this.cart += 1
     },
-    updateImage(variantImage) {
-      this.image = variantImage
+    updateVariant(index) {
+      this.selectedVariant = index
     }
   },
   data() {
@@ -73,7 +73,8 @@ export default {
       brand: "BeaverNamnam",
       product: "Mushroom and cheese tart",
       description: "Parmesan cheese, dried basil, fresh thyme and a clove of garlic served in a soft puff pastry",
-      image: require("../assets/images/1456.png"),
+      // image: require("../assets/images/1456.png"),
+      selectedVariant: 0,
       myPath: "http://www.w3schools.com",
       inventory: 100,
       onSale: true,
@@ -87,16 +88,22 @@ export default {
         "salt"
       ],
       variants: [
-        { id: '1456S', size: "S", image: require("../assets/images/1456S.png"), color: 'blue' },
-        { id: '1456M', size: "M", image: require("../assets/images/1456M.png"), color: 'green' },
+        { id: '14', size: "S", image: require("../assets/images/1456S.png"), color: 'blue', quantity: 50 },
+        { id: '15', size: "M", image: require("../assets/images/1456M.png"), color: 'green', quantity: 0 },
       ],
       counter: 0,
       cart: 0,
     };
   },
-  computed:{
-    title(){
+  computed: {
+    title() {
       return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].image
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity
     }
   }
 };
