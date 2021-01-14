4bd50af3e2c593b036e540e7c58b1d9a44faddfa
unknown <dgloria.web@gmail.com>
2021-01-14 01:39:10 +0000

    ep4

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,26 +1,29 @@
 <template>
   <div>
     <!-- <div class="product-image"> -->
-<img :src="image" alt="product image">
+    <img :src="image" alt="product image" />
     <!-- </div> -->
-    <h1>{{product}}</h1>
-    <p>{{description}}</p>
+    <h1>{{ product }}</h1>
+    <p>{{ description }}</p>
     <a :href="myPath" target="_blank">W3Schools</a>
+    <p v-if="inventory > 10">In Stock</p>
+    <p v-else-if="inventory <= 10 && inventory >0">Low Stock</p>
+    <p v-else>Out of Stock</p>
   </div>
 </template>
 
 <script>
 export default {
-  
   methods: {},
-  data(){
-    return{
-      product:'Boots',
-      description: 'these are boots',
+  data() {
+    return {
+      product: "Boots",
+      description: "these are boots",
       image: require("../assets/images/1456.png"),
-      myPath: 'http://www.w3schools.com'
-    }
-  }
+      myPath: "http://www.w3schools.com",
+      inventory: 100
+    };
+  },
 };
 </script>
 
