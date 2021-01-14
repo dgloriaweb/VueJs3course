c986d16b4f8389bb3460785f0ed5d0f53010f932
unknown <dgloria.web@gmail.com>
2021-01-14 02:14:49 +0000

    ep6 pt1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,49 @@
 <template>
   <div>
-    <!-- <div class="product-image"> -->
-    <img :src="image" alt="product image" />
-    <!-- </div> -->
-    <h1>{{ product }}</h1>
-    <p>{{ description }}</p>
-    <a :href="myPath" target="_blank">W3Schools</a>
-    <p v-if="inventory > 10">In Stock</p>
-    <p v-else-if="inventory <= 10 && inventory > 0">Low Stock</p>
-    <p v-else>Out of Stock</p>
-    <p v-if="onSale">OnSale</p>
-    <h2>Sensitivities</h2>
-    <ul>
-      <li v-for="sensitivity in sensitivities" :key="sensitivity.id">
-        {{ sensitivity }}
-      </li>
-    </ul>
-    <h2>nutritions</h2>
-    <ul>
-      <li v-for="nutrition in nutritions" :key="nutrition.id">
-        {{ nutrition }}
-      </li>
-    </ul>
-    <h2>Variants</h2>
-    <div class="" v-for="variant in variants" :key="variant.id">
-      {{ variant.size }}
+    <div class="container">
+      <div class="cart">Basic counter({{ counter }})</div>
+      <div class="cart">Cart({{ cart }})</div>
+      <!-- <div class="product-image"> -->
+      <img :src="image" alt="product image" />
+      <!-- </div> -->
+      <h1>{{ product }}</h1>
+      <p>{{ description }}</p>
+      <a :href="myPath" target="_blank">W3Schools</a>
+      <p v-if="inventory > 10">In Stock</p>
+      <p v-else-if="inventory <= 10 && inventory > 0">Low Stock</p>
+      <p v-else>Out of Stock</p>
+      <p v-if="onSale">OnSale</p>
+      <h2>Sensitivities</h2>
+      <ul>
+        <li v-for="sensitivity in sensitivities" :key="sensitivity.id">
+          {{ sensitivity }}
+        </li>
+      </ul>
+      <h2>nutritions</h2>
+      <ul>
+        <li v-for="nutrition in nutritions" :key="nutrition.id">
+          {{ nutrition }}
+        </li>
+      </ul>
+      <h2>Variants</h2>
+      <div class="" v-for="variant in variants" :key="variant.id">
+        {{ variant.size }}
+      </div>
+
+      <button class="button" type="button" v-on:click="counter += 1">Basic counter</button>
+      <button class="button" type="button" v-on:click="addToCart">Add to Cart</button>
+      <!-- end of container -->
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {},
+  methods: {
+    addToCart(){
+      this.cart +=1
+    }
+  },
   data() {
     return {
       product: "Mushroom and cheese tart",
@@ -43,32 +55,10 @@ export default {
       sensitivities: [
         "vegetarian",
         "vegan",
-        "dairyfree",
-        "glutenfree",
-        "halal",
-        "pregnant",
-        "wheat",
-        "crustaceans",
-        "eggs",
-        "fish",
-        "peanuts",
-        "soybeans",
-        "milk",
-        "nuts",
-        "celery",
-        "mustard",
-        "sesame",
-        "sulphurDioxide",
-        "lupin",
         "molluscs"],
       nutritions: [
         "weight",
         "energykj",
-        "energykcal",
-        "protein",
-        "carbohydrate",
-        "sugars",
-        "fat",
         "saturates",
         "fibre",
         "sodium",
@@ -77,7 +67,9 @@ export default {
       variants: [
         { id: '1456S', size: "S" },
         { id: '1456M', size: "M" },
-      ]
+      ],
+      counter:0,
+      cart: 0,
     };
   },
 };
